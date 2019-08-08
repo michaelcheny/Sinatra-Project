966eb0ff2186222ec1bdd6e827058245b24c2798
Michael Chen <michaelchengaming@gmail.com>
2019-08-07 22:45:36 -0700

    fix password input

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -16,7 +16,7 @@ class UserController < ApplicationController
   post '/login' do
     user = User.find_by(username: params[:username])
 
-    if user && user.authenticate(params[:password])
+    if !!user && user.authenticate(params[:password])
       session[:user_id] = user.id
       erb :'/meals/index'
     else

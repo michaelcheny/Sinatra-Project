203094906aa12f49f89a4d7316a268a96977f299
Michael Chen <michaelchengaming@gmail.com>
2019-08-07 20:40:35 -0700

    validates name lenght, username uniqueness, and password between 6 and 20

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :workouts
   # has many :
   has_secure_password
-  validates :name, presence: true
+  validates :name, length: { minimum: 2 }, presence: true
   validates :username, presence: true, uniqueness: true
+  validates :password, length: { in: 6..20 }
 end

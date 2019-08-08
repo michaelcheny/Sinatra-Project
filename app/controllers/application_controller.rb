b89bfab55b544d06d3c4b907dc96b90727c34d6a
Michael Chen <michaelchengaming@gmail.com>
2019-08-08 16:00:38 -0700

    add comments throughout the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,34 +1,38 @@
 class ApplicationController < Sinatra::Base
 
+  ## sets everything up
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-    enable :sessions
-    set :session_secret, SESSION_SECRET
+    set :public_folder, 'public'  ## sets the location for public folder
+    set :views, 'app/views'   ## sets the location for views folder
+    enable :sessions  ## allows us to have sessions
+    set :session_secret, SESSION_SECRET  ## sets the secret hex key for password security
   end
 
-
+  ## the very first page of our app. Kind of like our welcome page that leads to log in or registration
   get '/' do
     erb :index
   end
 
-
+  ## helper methods for our controllers
   helpers do
 
+    ## checks whether our user is logged in or not for the current session
     def logged_in?
       !!session[:user_id]
     end
 
+    ## gives us the current user to work with
     def current_user
       User.find_by(id: session[:user_id])
     end
 
+    ## checks to see if user is authorized. If they're not logged in, they get redirected to log in page
     def authenticate
       if !logged_in?
         redirect '/login'
       end
     end
-    
+
   end
 
   
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,28 @@
 class SessionsController < ApplicationController
 
-
+  ## Home page for the user. Checks to see if user is logged in, if not, will redirect to login page ##
   get '/home' do
-    @users = User.all
+    # @users = User.all
     authenticate
+
     erb :'/users/home'
   end
 
-
+  ## Registration page, allows user to create a new account ##
   get '/users/new' do
     erb :"/users/register"
   end
 
-
+  ## Log in page for user. If user is already logged in, redirects them to home page. ##
   get '/login' do
     redirect '/home' if logged_in? 
 
-    @failed = false
+    @failed = false ## for error message
+
     erb :'/users/login'
   end
 
-
+  ## Post request for the log in form. If user logs in correctly, we set their current session user_id and redirect them to their homepage. If failed log in, we bring them back to log in page and show error message.
   post '/login' do
     @user = User.find_by(username: params[:username])
 
@@ -28,11 +30,11 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect "/home"
     else
-      @failed = true
+      @failed = true ## for error message
       erb :'/users/login'
     end
 
   end
   
-  
+
 end

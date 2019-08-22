c76192fd600f0247a6336a3c0fcd996b58154754
Michael Chen <michaelchengaming@gmail.com>
2019-08-22 00:46:24 -0700

    fix css so margin-top for body isnt too far down. add a different greeting for imcomplete users

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,5 +1,6 @@
 class Meal < ActiveRecord::Base
   belongs_to :user
 
+  validates :name, format: { with: /\A[a-zA-Z0-9_ -]+\z/, message: "cannot contain symbols" }
   validates :calories, numericality: { only_integer: true }
 end
